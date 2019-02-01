8e5c202e5e2e263d8ebc46c66fccb5a0b4da890f
Johannes Strauß <jstrauss@evoila.de>
2019-02-01 14:19:37 +0100

    fix(): secured packages access now handled in elasticsearch

--- src/main/kotlin/de/evoila/bpm/custom/elasticsearch/repositories/CustomVendorRepository.kt
+++ src/main/kotlin/de/evoila/bpm/custom/elasticsearch/repositories/CustomVendorRepository.kt
@@ -1,14 +1,12 @@
 package de.evoila.bpm.custom.elasticsearch.repositories
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import de.evoila.bpm.custom.elasticsearch.ElasticSearchRestTemplate
 import de.evoila.bpm.entities.Vendor
 import kotlinx.serialization.json.Json
 import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.index.query.BoolQueryBuilder
 import org.elasticsearch.index.query.MatchQueryBuilder
-import org.elasticsearch.index.query.QueryBuilders
 import org.elasticsearch.search.builder.SearchSourceBuilder
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -22,18 +20,6 @@ class CustomVendorRepository(
   }
 
   override val index: String = "vendors"
-
-  override fun findAll(): List<Vendor> {
-    val searchSourceBuilder = SearchSourceBuilder()
-    searchSourceBuilder.query(QueryBuilders.matchAllQuery())
-    val searchRequest = SearchRequest(index, type)
-    searchRequest.source(searchSourceBuilder)
-    val response = elasticSearchRestTemplate.performSearchRequest(searchRequest)
-
-    return response.hits.map {
-      Json.parse(Vendor.serializer(), it.sourceAsString)
-    }
-  }
 
   override fun findById(id: String): Optional<Vendor> {
     val response = requestById(id)
@@ -49,8 +35,9 @@ class CustomVendorRepository(
 
   fun findByName(name: String): Vendor? {
     val searchSourceBuilder = SearchSourceBuilder()
-    searchSourceBuilder.query(MatchQueryBuilder("name", name))
-
+    val boolQueryBuilder = BoolQueryBuilder()
+    boolQueryBuilder.must(MatchQueryBuilder("name", name))
+    searchSourceBuilder.query(boolQueryBuilder)
 
     val searchRequest = SearchRequest().indices(index).types(type).source(searchSourceBuilder)
     val response = elasticSearchRestTemplate.performSearchRequest(searchRequest)
@@ -58,7 +45,14 @@ class CustomVendorRepository(
     return response.hits.map { Json.parse(Vendor.serializer(), it.sourceAsString) }.firstOrNull()
   }
 
-  companion object {
-    private val log = LoggerFactory.getLogger(CustomPackageRepository::class.java)
+  fun memberOfByName(username: String): List<Vendor> {
+
+    val searchSourceBuilder = SearchSourceBuilder()
+    searchSourceBuilder.query(MatchQueryBuilder("members", username))
+
+    val searchRequest = SearchRequest().indices(index).types(type).source(searchSourceBuilder)
+    val response = elasticSearchRestTemplate.performSearchRequest(searchRequest)
+
+    return response.hits.map { Json.parse(Vendor.serializer(), it.sourceAsString) }
   }
 }
